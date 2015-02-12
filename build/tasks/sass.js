aa0fbc2bb2bd9b4cc1be3029adc05ee09a79d91d
Benjamin Cochrane <ben.cochrane2112@gmail.com>
2015-02-12 11:28:23 -0500

    Fixed a small scss bug and re-wrote sass compilation step

--- build/tasks/sass.js
+++ build/tasks/sass.js
@@ -1,5 +1,12 @@
-var sass = require('compass')
+/*var sass = require('compass')
 
 module.exports = function(cb) {
   sass.compile(cb)
+}
+*/
+
+var exec = require('child_process').exec;
+
+module.exports = function(cb) {
+  exec('compass compile', function() {cb();});
 }
